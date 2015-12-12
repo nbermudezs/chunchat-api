7ecb079d909287381b550f22c486e5c4151aec5f
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2015-12-12 16:38:11 -0600

    (setup) remove console.log

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -2,9 +2,6 @@ var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
   'mongodb://localhost/main';
 
-console.log('MONGOLAB_URI ENV VAR: ');
-console.log(process.env.MONGOLAB_URI);
-
 module.exports = {
   mongodb: {
     defaultForType: "mongodb",
