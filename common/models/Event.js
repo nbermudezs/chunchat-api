a540390571debdcd18ae10e70d528b3e07e026c1
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2015-12-12 16:37:52 -0600

    (events) add remote methods to start and close the event

--- common/models/Event.js
+++ common/models/Event.js
@@ -1,3 +1,45 @@
 module.exports = function(Event) {
+  Event.statuses = {
+    CREATED: 'CREATED',
+    STARTED: 'STARTED',
+    CLOSED : 'CLOSED'
+  };
 
+  function updateStatus(eventId, newStatus, cb) {
+    Event.findById( eventId, function (err, instance) {
+      if (err) return cb(err);
+      if (!instance) return cb(null, null);
+
+      instance.updateAttribute('status', newStatus, function(err, updated) {
+        var response = updated && updated.status;
+        cb(null, response);
+      });
+    });
+  }
+
+  Event.start = function(eventId, cb) {
+    updateStatus(eventId, Event.statuses.STARTED, cb);
+  }
+
+  Event.close = function(eventId, cb) {
+    updateStatus(eventId, Event.statuses.CLOSED, cb);
+  }
+
+  Event.remoteMethod (
+    'start',
+    {
+      http: { path: '/start', verb: 'get' },
+      accepts: { arg: 'id', type: 'string', http: { source: 'query' } },
+      returns: { arg: 'status', type: 'string' }
+    }
+  );
+
+  Event.remoteMethod (
+    'close',
+    {
+      http: { path: '/close', verb: 'get' },
+      accepts: { arg: 'id', type: 'string', http: { source: 'query' } },
+      returns: { arg: 'status', type: 'string' }
+    }
+  );
 };
