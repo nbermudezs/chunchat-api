8e827c99ff6ce986de1aca522a955c1b2998cce4
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2015-12-12 22:18:44 -0600

    (users) add role info to default users

--- server/boot/seed-people.js
+++ server/boot/seed-people.js
@@ -6,15 +6,51 @@ module.exports = function(app, cb) {
    * http://docs.strongloop.com/display/public/LB/Working+with+LoopBack+objects
    * for more info.
    */
+  var RoleMapping = app.models.RoleMapping;
+  var Person = app.models.Person;
+  var Role = app.models.Role;
+
   var users = [
     {
       firstName: 'Nestor',
-      email: 'nestor.bermudez@agilityfeat.com',
+      email: 'nestor.bermudez+dev@agilityfeat.com',
+      password: 'AGILITYfeat1'
+    },
+    {
+      firstName: 'Nestor',
+      email: 'nestor.bermudez+user@agilityfeat.com',
+      password: 'AGILITYfeat1'
+    },
+    {
+      firstName: 'Nestor',
+      email: 'nestor.bermudez+admin@agilityfeat.com',
       password: 'AGILITYfeat1'
     }
   ];
 
-  app.models.Person.create(users, function(err, response) {
+  Person.create(users, function(err, users) {
+    if (err) return cb();
+
+    Role.create({
+      name: 'admin'
+    }, function (err, role) {
+      if (err || !role) return;
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: users[2].id
+      })
+    });
+
+    Role.create({
+      name: 'developer'
+    }, function (err, role) {
+      if (err || !role) return;
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: users[0].id
+      })
+    });
+
     cb();
   })
 };
