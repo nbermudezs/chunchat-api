d65d476be82d85e6e1fdb315aa3867dbd72a3bb9
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2015-12-12 21:57:28 -0600

    (email) add sendgrid setup

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -7,5 +7,11 @@ module.exports = {
     defaultForType: "mongodb",
     connector: "mongodb",
     url: mongoUri
+  },
+
+  sendgrid: {
+    connector: "loopback-connector-sendgrid",
+    api_user: process.env.SENDGRID_API_USER,
+    api_key: process.env.SENDGRID_API_KEY
   }
 };
