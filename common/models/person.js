2cbba4ac0a433526c0c2b6863b60186fd1e250cb
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2015-12-12 20:44:47 -0600

    (users) add beforeRemote hook to add ttl

--- common/models/person.js
+++ common/models/person.js
@@ -1,3 +1,11 @@
 module.exports = function(Person) {
 
+  // Add expiration to the user token.
+  var TWO_WEEKS = 60 * 60 * 24 * 7 * 2;
+  Person.beforeRemote('login', function(context, user, next) {
+    var req = context.req;
+    req.body.ttl = TWO_WEEKS;
+    next();
+  });
+
 };
